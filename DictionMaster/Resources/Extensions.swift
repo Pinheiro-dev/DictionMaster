752a1ad285913b119a7e4e2d58900aa90e8d427e
Pinheiro-dev <mathepba@gmail.com>
2024-02-09 00:30:07 -0300

    Feat: Cria extension de date e cria classe DMUSerDefaultsManager

--- DictionMaster/Resources/Extensions.swift
+++ DictionMaster/Resources/Extensions.swift
@@ -123,3 +123,15 @@ extension UIViewController{
         self.present(alert, animated: true)
     }
 }
+
+//MARK: - Date
+
+extension Date {
+    func get(_ components: Calendar.Component..., calendar: Calendar = Calendar.current) -> DateComponents {
+        return calendar.dateComponents(Set(components), from: self)
+    }
+
+    func get(_ component: Calendar.Component, calendar: Calendar = Calendar.current) -> Int {
+        return calendar.component(component, from: self)
+    }
+}
